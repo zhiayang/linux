ecd918ba9b82b579046e900238c958c53adfe477
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-03-04 10:55:40 +0200

    iio: buffer: Switch to bitmap_zalloc()
    Switch to bitmap_zalloc() to show clearly what we are allocating.
    Besides that it returns pointer of bitmap type instead of opaque void *.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit 7c1159f0cf1e726fb28d5b6d40766dd4ac0aafad)

--- drivers/iio/industrialio-buffer.c
+++ drivers/iio/industrialio-buffer.c
@@ -399,8 +399,7 @@ static int iio_channel_mask_set(struct iio_dev *indio_dev,
 	unsigned long *trialmask;
 	unsigned int ch;
 
-	trialmask = kcalloc(BITS_TO_LONGS(indio_dev->masklength),
-			    sizeof(*trialmask), GFP_KERNEL);
+	trialmask = bitmap_zalloc(indio_dev->masklength, GFP_KERNEL);
 	if (trialmask == NULL)
 		return -ENOMEM;
 	if (!indio_dev->masklength) {
