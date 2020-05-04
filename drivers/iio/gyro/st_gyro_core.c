f17976f1186fede0af2e889db22400e2b46975e8
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2020-05-04 06:15:00 +0300

    iio: common: st_gyro_core: Replace indio_dev->mlock with own device lock
    As part of the general cleanup of indio_dev->mlock, this change replaces
    it with a local lock on the device's state structure.
    This also changes st_gyro_write_raw function to pass the pointer to the
    state-struct in order to access the lock.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- drivers/iio/gyro/st_gyro_core.c
+++ drivers/iio/gyro/st_gyro_core.c
@@ -390,6 +390,7 @@ static int st_gyro_write_raw(struct iio_dev *indio_dev,
 		struct iio_chan_spec const *chan, int val, int val2, long mask)
 {
 	int err;
+	struct st_sensor_data *gdata = iio_priv(indio_dev);
 
 	switch (mask) {
 	case IIO_CHAN_INFO_SCALE:
@@ -398,9 +399,9 @@ static int st_gyro_write_raw(struct iio_dev *indio_dev,
 	case IIO_CHAN_INFO_SAMP_FREQ:
 		if (val2)
 			return -EINVAL;
-		mutex_lock(&indio_dev->mlock);
+		mutex_lock(&gdata->lock);
 		err = st_sensors_set_odr(indio_dev, val);
-		mutex_unlock(&indio_dev->mlock);
+		mutex_unlock(&gdata->lock);
 		return err;
 	default:
 		err = -EINVAL;
