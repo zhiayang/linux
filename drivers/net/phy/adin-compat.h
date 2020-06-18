c9a6b31fc5f2e35c046ed353b79b38ae30201b78
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-06-18 10:42:13 +0300

    net: phy: adin-t1l: extend compat layer with some C45 logic
    Some C45 functions that are needed in the ADIN1100 driver are only present
    in newer kernels.
    So, we need to backport them to the ADI 4.19 version.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/net/phy/adin-compat.h
+++ drivers/net/phy/adin-compat.h
@@ -64,3 +64,53 @@ static inline int phy_set_bits_mmd(struct phy_device *phydev, int devad,
 {
 	return phy_modify_mmd(phydev, devad, regnum, 0, val);
 }
+
+/**
+ * Below are parts for ADIN1100 T1 PHY, for Clause 45 that are missing in 4.19
+ * Defining them here as static-inline is a trick to get them working until
+ * the kernel is updated, and they are present.
+ * Same as with other parts from this file, these are backports re-implemented
+ * or adapted for this kernel version.
+ */
+
+/*
+ * Careful with genphy_c45_read_link() : return value
+ * semantic differs slightly in 4.19 vs newer kernels.
+ * In newer kernels, it returns 0 or negative for error.
+ * In 4.19-ish, it also eturns true if the link is active.
+ */
+static inline int genphy_c45_read_link_compat(struct phy_device *phydev)
+{
+	int ret = genphy_c45_read_link(phydev, MDIO_DEVS_PMAPMD);
+
+	if (ret < 0) {
+		phydev->link = 0;
+		return ret;
+	}
+
+	phydev->link = (ret == true);
+
+	return 0;
+}
+
+#define genphy_c45_read_link	genphy_c45_read_link_compat
+
+static inline int genphy_c45_check_and_restart_aneg(struct phy_device *phydev, bool restart)
+{
+	int ret;
+
+	if (!restart) {
+		/* Configure and restart aneg if it wasn't set before */
+		ret = phy_read_mmd(phydev, MDIO_MMD_AN, MDIO_CTRL1);
+		if (ret < 0)
+			return ret;
+
+		if (!(ret & MDIO_AN_CTRL1_ENABLE))
+			restart = true;
+	}
+
+	if (restart)
+		return genphy_c45_restart_aneg(phydev);
+
+	return 0;
+}
